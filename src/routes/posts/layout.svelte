bd0d18f23d45768d65e27eb8e77e35548299b0db
Ian Pratt <thingcoder1@gmail.com>
2022-02-11 15:30:28 +0000

    ok katex css in global stylesheet

--- src/routes/posts/layout.svelte
+++ src/routes/posts/layout.svelte
@@ -6,12 +6,14 @@
 <svelte:head>
   <title>Ian Pratt | {title}</title>
   <meta property="og:title" content={"Ian Pratt | " + title} />
+  <!--
   <link
     rel="stylesheet"
     href="https://cdn.jsdelivr.net/npm/katex@0.12.0/dist/katex.min.css"
     integrity="sha384-AfEj0r4/OFrOo5t7NnNe46zW/tFgW6x/bCJG8FqQCEo3+Aro6EYUG4+cU+KJWu/X"
     crossorigin="anonymous"
   />
+  -->
 </svelte:head>
 
 <h1>{title}</h1>
