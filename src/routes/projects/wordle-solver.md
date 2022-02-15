3877ba60465786102298532c441c34c66c3205ef
Ian Pratt <ianjdpratt@gmail.com>
2022-02-14 21:36:18 -0600

    asdfasdf

--- src/routes/projects/wordle-solver.md
+++
@@ -1,4 +0,0 @@
----
-title: A Wordle solver
-date: 2022-02-14
----
---
+++ src/routes/projects/wordle-solver.svelte
@@ -0,0 +1,17 @@
+<script context="module">
+  export const metadata = {
+    title: "A Wordle solver",
+    date: "2022-02-14",
+  };
+</script>
+
+<script>
+  import fs from "fs";
+
+  let data = fs.readFileSync(
+    "../../lib/projects/wordle-solver/words.txt",
+    "utf8"
+  );
+</script>
+
+{data}
