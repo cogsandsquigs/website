babc0166f98d44610523c9e62ade80b779f16c36
Ian Pratt <ianjdpratt@gmail.com>
2022-04-22 18:09:40 -0500

    fixing up sitemap and rss feed

--- src/routes/sitemap.xml.js
+++ src/routes/sitemap.xml.js
@@ -1,17 +1,17 @@
-import { website } from "$lib/info"
+import { website } from "$lib/info";
 
-import { projects } from "$lib/projects"
-import { posts } from "$lib/posts"
-import { pages } from "$lib/pages"
+import { projects } from "$lib/projects";
+import { posts } from "$lib/posts";
+import { pages } from "$lib/pages";
 
 export async function get() {
-	const headers = {
-		"Cache-Control": "max-age=0, s-maxage=3600",
-		"Content-Type": "application/xml",
-	}
-	return {
-		headers,
-		body: `<?xml version="1.0" encoding="UTF-8" ?>
+  const headers = {
+    "Cache-Control": "max-age=0, s-maxage=3600",
+    "Content-Type": "application/xml",
+  };
+  return {
+    headers,
+    body: `<?xml version="1.0" encoding="UTF-8" ?>
     <urlset
       xmlns="https://www.sitemaps.org/schemas/sitemap/0.9"
       xmlns:news="https://www.google.com/schemas/sitemap-news/0.9"
@@ -22,46 +22,47 @@ export async function get() {
     >
 
       ${(await pages())
-			.map((page) =>
-				page.isPrivate
-					? null
-					: `
+        .map((page) =>
+          page.isPrivate
+            ? null
+            : `
         <url>
           <loc>${website}/${page.slug}</loc>
           <changefreq>daily</changefreq>
           <priority>0.7</priority>
         </url>
-        `,
-			)
-			.join("")}
+        `
+        )
+        .join("")}
       <!--
       ${(await projects())
-			.map((post) =>
-				post.isPrivate
-					? null
-					: `
+        .map((post) =>
+          post.isPrivate
+            ? null
+            : `
         <url>
           <loc>${website}/projects/${post.slug}</loc>
           <changefreq>daily</changefreq>
           <priority>0.7</priority>
         </url>
-        `,
-			)
-			.join("")}
+        `
+        )
+        .join("")}
       -->  
       ${(await posts())
-			.map((post) =>
-				post.isPrivate
-					? null
-					: `
+        .map((x) => x.metadata)
+        .map((post) =>
+          post.isPrivate
+            ? null
+            : `
       <url>
         <loc>${website}/blog/${post.slug}</loc>
         <changefreq>daily</changefreq>
         <priority>0.7</priority>
       </url>
-      `,
-			)
-			.join("")}
+      `
+        )
+        .join("")}
     </urlset>`,
-	}
+  };
 }
