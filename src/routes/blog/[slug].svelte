80e3a4a04659f0577d23a34dedd08d0307a82019
Ian Pratt <ianjdpratt@gmail.com>
2022-04-21 19:11:52 -0500

    push rn

---
+++ src/routes/blog/[slug].svelte
@@ -0,0 +1,26 @@
+<script context="module">
+  export async function load({ fetch, url, params }) {
+    console.log(params.slug);
+
+    const files = import.meta.glob("/posts/*.md");
+
+    console.log(files);
+
+    let pages = [];
+
+    for (file in files) {
+      pages.push(await files[file]().then(({ metadata }) => metadata));
+    }
+
+    console.log(pages);
+
+    return {
+      props: {},
+    };
+  }
+</script>
+
+<script>
+  export let render;
+  export let metadata;
+</script>
