a84134ef8fe222a0d6f8823ba075a39923880762
Ian Pratt <thingcoder1@gmail.com>
2022-05-19 16:57:53 +0000

    eh undo some stuff

--- src/lib/components/Nav.svelte
+++ src/lib/components/Nav.svelte
@@ -1,14 +1,34 @@
 <script lang="ts">
-  import DropdownMenu from "$lib/components/DropdownMenu.svelte";
+  import {
+    Popover,
+    PopoverButton,
+    PopoverPanel,
+  } from "@rgossiaux/svelte-headlessui";
 </script>
 
 <nav class="space-y-0">
   <div class="px-4 py-1 flex box-border justify-between items-center">
-    <DropdownMenu>
-      <a href="/">Index</a>
-      <a href="/blog">Blog</a>
-      <a href="https://github.com/cogsandsquigs">Github</a>
-    </DropdownMenu>
+    <Popover
+      class="text-left text-md sm:text-lg md:text-xl max-w-auto"
+      style="position: relative;"
+    >
+      <PopoverButton
+        class="font-bold underline decoration-secondary decoration-2"
+      >
+        Menu
+      </PopoverButton>
+
+      <PopoverPanel style="position: absolute; z-index: 10;">
+        <div
+          class="bg-primary p-3 border-2 border-secondary rounded-sm panel-contents"
+        >
+          <a href="/">Index</a>
+          <a href="/blog">Blog</a>
+          <a href="https://github.com/cogsandsquigs">Github</a>
+        </div>
+      </PopoverPanel>
+    </Popover>
+
     <div class="text-right text-xs sm:text-sm md:text-lg space-y-0">
       <h2 class="m-0">Ian Pratt</h2>
       <h3>Just a gay programmer</h3>
