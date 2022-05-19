457bc4839e531dc2e6e226581508c1fd87962746
Ian Pratt <thingcoder1@gmail.com>
2022-05-19 15:27:57 +0000

    ADDED A POPOVER FOR THE MENU !!!

--- src/lib/components/Nav.svelte
+++ src/lib/components/Nav.svelte
@@ -1,9 +1,33 @@
+<script lang="ts">
+  import {
+    Popover,
+    PopoverButton,
+    PopoverPanel,
+  } from "@rgossiaux/svelte-headlessui";
+</script>
+
 <nav class="space-y-0">
   <div class="px-4 py-1 flex box-border justify-between items-center">
-    <div class="text-left text-md sm:text-lg md:text-xl max-w-auto">
-      <a href="/">Index</a>
-      <a href="/blog">Blog</a>
-    </div>
+    <Popover
+      class="text-left text-md sm:text-lg md:text-xl max-w-auto"
+      style="position: relative;"
+    >
+      <PopoverButton
+        class="font-bold underline decoration-secondary decoration-2"
+      >
+        Menu
+      </PopoverButton>
+
+      <PopoverPanel style="position: absolute; z-index: 10;">
+        <div
+          class="bg-primary p-3 border-2 border-secondary rounded-sm panel-contents"
+        >
+          <a href="/">Index</a>
+          <a href="/blog">Blog</a>
+        </div>
+      </PopoverPanel>
+    </Popover>
+
     <div class="text-right text-xs sm:text-sm md:text-lg space-y-0">
       <h2 class="m-0">Ian Pratt</h2>
       <h3>Just a gay programmer</h3>
