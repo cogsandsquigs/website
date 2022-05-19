9c6b7674fc93d1f0ed44f896c397e76715b980d7
Ian Pratt <thingcoder1@gmail.com>
2022-05-19 16:52:56 +0000

    just extracted some components

--- src/lib/components/Nav.svelte
+++ src/lib/components/Nav.svelte
@@ -1,34 +1,14 @@
 <script lang="ts">
-  import {
-    Popover,
-    PopoverButton,
-    PopoverPanel,
-  } from "@rgossiaux/svelte-headlessui";
+  import DropdownMenu from "$lib/components/DropdownMenu.svelte";
 </script>
 
 <nav class="space-y-0">
   <div class="px-4 py-1 flex box-border justify-between items-center">
-    <Popover
-      class="text-left text-md sm:text-lg md:text-xl max-w-auto"
-      style="position: relative;"
-    >
-      <PopoverButton
-        class="font-bold underline decoration-secondary decoration-2"
-      >
-        Menu
-      </PopoverButton>
-
-      <PopoverPanel style="position: absolute; z-index: 10;">
-        <div
-          class="bg-primary p-3 border-2 border-secondary rounded-sm panel-contents"
-        >
-          <a href="/">Index</a>
-          <a href="/blog">Blog</a>
-          <a href="https://github.com/cogsandsquigs">Github</a>
-        </div>
-      </PopoverPanel>
-    </Popover>
-
+    <DropdownMenu>
+      <a href="/">Index</a>
+      <a href="/blog">Blog</a>
+      <a href="https://github.com/cogsandsquigs">Github</a>
+    </DropdownMenu>
     <div class="text-right text-xs sm:text-sm md:text-lg space-y-0">
       <h2 class="m-0">Ian Pratt</h2>
       <h3>Just a gay programmer</h3>
