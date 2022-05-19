f448570a9eaca229cf55fb4bb7d1ba5460b0cd58
Ian Pratt <thingcoder1@gmail.com>
2022-05-19 15:05:28 +0000

    edited fonts yay

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,10 +2,17 @@ module.exports = {
   important: true,
   content: ["./src/**/*.{html,js,svelte,ts}"],
   theme: {
+
     colors: {
       primary: "#030D14",
       secondary: "#0284c7",
     },
+
+    fontFamily: {
+      display: ['IBM Plex Mono', 'Menlo', 'monospace'],
+      body: ['IBM Plex Mono', 'Menlo', 'monospace'],
+    },
+
     extend: {},
   },
   plugins: [require("@tailwindcss/typography")],
