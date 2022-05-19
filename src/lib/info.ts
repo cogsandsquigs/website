9afd05258e3a346edae03b4a7ac858df1ce1c2aa
Ian Pratt <thingcoder1@gmail.com>
2022-05-19 16:47:48 +0000

    added meta tag for description

--- src/lib/info.ts
+++ src/lib/info.ts
@@ -1,5 +1,5 @@
 export const title = "Ian Pratt | Just a gay programmer"
 export const website = "https://ianpratt.gq"
-export const description = "Ian Pratt's little corner of the web!"
+export const description = "My little corner of the web!"
 export const locale: string = "en-US";
 export const dateOptions: Intl.DateTimeFormatOptions = { weekday: "long", year: "numeric", month: "long", day: "numeric" };
--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,8 +1,12 @@
 <script>
   import "$lib/styles/globals.css";
+  import { description } from "$lib/info";
   import Nav from "$lib/components/Nav.svelte";
 </script>
 
+<svelte:head>
+  <meta name="description" content={description} />
+</svelte:head>
 <body
   class="font-body px-0.5 sm:px-1.5 md:px-3.5 pt-1 bg-primary prose prose-invert prose-a:underline prose-a:decoration-secondary prose-a:decoration-2 prose-a:font-bold prose-hr:border-[1px] prose-hr:border-secondary max-w-none"
 >
