c7efa3e548009be4c8de1bf8ebb74e2fc1b635d3
Ian Pratt <thingcoder1@gmail.com>
2022-05-20 13:48:39 +0000

    edited more pages!

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -1,10 +1,42 @@
 <script>
-    import profile from "$lib/images/profile.png";
+    import profileimg from "$lib/images/profile.png";
 </script>
 
-<h1 class="text-center">About Me</h1>
+<h1 class="text-center transform duration-300 hover:-rotate-12">About Me</h1>
 <img
-    class="w-20 md:w-32 mx-auto md:m-0 md:p-0 md:space-x-0"
-    src={profile}
-    alt="My profile photo."
+    class="w-20 sm:w-28 md:w-32 lg:w-40 mx-auto border-2 border-secondary rounded-full"
+    src={profileimg}
+    alt="My profile."
 />
+
+<p>
+    Hey! I'm Ian Pratt, although I go by the online alias <u>cogsandsquigs</u>.
+    I program things for fun and profit (although mainly for fun!). Currently,
+    my main project is working on my website, and making it look as brilliant
+    and fancy as possible :3. It's also a great way to practice my
+    <a href="/blog/tags/webdev">webdev</a> skills!
+</p>
+
+<p>
+    It may be obvious at this point, but I built this whole site from scratch! I
+    really like programming things (especially things I can see!), and it's what
+    I plan to have a job in in the future.
+</p>
+
+<p>
+    And of course, I'm <em>fabulously</em> gay. Don't worry though, I don't bite!
+    (at least, in most cases I don't).
+</p>
+
+<p>
+    Check out the rest of my site for more stuff I'm doing! Or, if it's your
+    thing, take a look at my <a href="https://github.com/cogsandsquigs"
+        >Github page</a
+    > if you're into looking at someone else's code.
+</p>
+
+<p>
+    (P.S.: If you're <em>really</em> into my blog, you might be happy to know
+    that it has an <a href="/rss.xml">RSS feed</a>! Enjoy live updates of my
+    blog every 1-2 millenia.)
+</p>
