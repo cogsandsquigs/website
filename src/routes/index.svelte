3814055cb66249195c798a2308f8ba6769228773
Ian Pratt <thingcoder1@gmail.com>
2022-05-23 16:40:52 -0500

    Update index.svelte

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -16,7 +16,7 @@
   <a href="https://kit.svelte.dev">SvelteKit</a> and
   <a href="https://tailwindcss.com">Tailwind CSS</a>. This website is also
   deployed on
-  <a href="https://fly.io">Fly.io</a>! In any case, I hope you enjoy what you
+  <a href="https://vercel.com">Vercel</a>! In any case, I hope you enjoy what you
   find here! Why not check out my
   <a href="blog/hello-world">testing page</a>? It's where I test out new things
   for my website, and generally just mess around!
