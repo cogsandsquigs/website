10e7b56983d32630be57a2ce5e090a387829b880
Ian Pratt <thingcoder1@gmail.com>
2022-05-23 19:07:44 +0000

    added xml declaration

--- src/routes/rss.xml.ts
+++ src/routes/rss.xml.ts
@@ -16,8 +16,8 @@ export async function get() {
 
 const xml = (
   posts
-) => `
-<rss xmlns:dc="https://purl.org/dc/elements/1.1/" xmlns:content="https://purl.org/rss/1.0/modules/content/" xmlns:atom="https://www.w3.org/2005/Atom" version="2.0">
+) => `<?xml version="1.0"?>
+<rss version="2.0">
   <channel>
     <title>${title}</title>
     <link>${website}</link>
