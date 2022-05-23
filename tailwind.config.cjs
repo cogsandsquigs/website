45a418021e5e6726d0929450714701dc9f36cb11
Ian Pratt <ianjdpratt@gmail.com>
2022-05-23 17:37:21 -0500

    updated font to use fira code mono

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,15 +2,28 @@ module.exports = {
   important: true,
   content: ["./src/**/*.{html,js,svelte,ts}"],
   theme: {
-
     colors: {
       primary: "#030D14",
       secondary: "#0284c7",
     },
 
     fontFamily: {
-      display: ['IBM Plex Mono', 'Menlo', 'monospace'],
-      body: ['IBM Plex Mono', 'Menlo', 'monospace'],
+      display: [
+        "Fira Code",
+        "Fira Mono",
+        "Menlo",
+        "Consolas",
+        "DejaVu Sans Mono",
+        "monospace",
+      ],
+      body: [
+        "Fira Code",
+        "Fira Mono",
+        "Menlo",
+        "Consolas",
+        "DejaVu Sans Mono",
+        "monospace",
+      ],
     },
 
     extend: {},
