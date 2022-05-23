5b5ff04258833c49dce2d63677487464a65f9994
Ian Pratt <thingcoder1@gmail.com>
2022-05-23 13:49:52 +0000

    more info

--- src/routes/about/info.svelte
+++ src/routes/about/info.svelte
@@ -60,4 +60,29 @@
             <DisclosurePanel>Mr. works just fine!</DisclosurePanel>
         </Transition>
     </Disclosure>
+    <Disclosure let:open class="space-y-2">
+        <DisclosureButton
+            class="flex justify-between min-w-full border-secondary border-2 rounded-sm pt-1 px-2"
+        >
+            <span>Okay! Anything you like?</span>
+            <ChevronUpIcon
+                style={open ? "transform: rotate(180deg);" : ""}
+                class="w-8 p-1 duration-150"
+            />
+        </DisclosureButton>
+        <Transition
+            enter="transition duration-100 ease-out"
+            enterFrom="transform scale-95 opacity-0"
+            enterTo="transform scale-100 opacity-100"
+            leave="transition duration-75 ease-out"
+            leaveFrom="transform scale-100 opacity-100"
+            leaveTo="transform scale-95 opacity-0"
+        >
+            <DisclosurePanel
+                >Cheesecakes of any and all varieties, elegant and <em
+                    >working</em
+                > code, and finally, sleep ;)
+            </DisclosurePanel>
+        </Transition>
+    </Disclosure>
 </div>
