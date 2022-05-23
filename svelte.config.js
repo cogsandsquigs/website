0333037ee5eec230ef39a467da59771b4b87939b
Ian Pratt <thingcoder1@gmail.com>
2022-05-23 16:25:04 -0500

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,6 @@
 import { mdsvex } from "mdsvex";
 import mdsvexconfig from "./mdsvex.config.js";
-import adapter from '@sveltejs/adapter-node';
+import adapter from '@sveltejs/adapter-auto';
 import preprocess from "svelte-preprocess";
 
 /** @type {import('@sveltejs/kit').Config} */
