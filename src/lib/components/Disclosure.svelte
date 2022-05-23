56b3158d040aedee19566812d37e5458f4b3fd98
Ian Pratt <thingcoder1@gmail.com>
2022-05-23 17:04:54 +0000

    change disclosure animations

--- src/lib/components/Disclosure.svelte
+++ src/lib/components/Disclosure.svelte
@@ -27,10 +27,10 @@
         />
     </DisclosureButton>
     <Transition
-        enter="transition duration-100 ease-out"
+        enter="transition duration-200 ease-out"
         enterFrom="transform scale-95 opacity-0"
         enterTo="transform scale-100 opacity-100"
-        leave="transition duration-75 ease-out"
+        leave="transition duration-200 ease-out"
         leaveFrom="transform scale-100 opacity-100"
         leaveTo="transform scale-95 opacity-0"
     >
