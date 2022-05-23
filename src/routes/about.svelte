c90bf0edcf63144817683f5f073d324039a8fd28
Ian Pratt <thingcoder1@gmail.com>
2022-05-23 15:38:52 +0000

    turned my disclosures into a component

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -1,13 +1,7 @@
-<script>
+<script lang="ts">
   import profileimg from "$lib/images/profile.png";
   import { title } from "$lib/info";
-  import {
-    Disclosure,
-    DisclosureButton,
-    DisclosurePanel,
-    Transition,
-  } from "@rgossiaux/svelte-headlessui";
-  import { ChevronUpIcon } from "@rgossiaux/svelte-heroicons/solid";
+  import Disclosure from "$lib/components/Disclosure.svelte";
 </script>
 
 <svelte:head>
@@ -46,148 +40,30 @@
 <p>Anyways, on to some rapid-fire information blasting!</p>
 
 <div class="space-y-4">
-  <Disclosure let:open class="space-y-2">
-    <DisclosureButton
-      class="flex justify-between min-w-full border-secondary border-2 rounded-sm pt-1 px-2"
-    >
-      <span>First off. Pronouns?</span>
-      <ChevronUpIcon
-        style={open ? "transform: rotate(180deg);" : ""}
-        class="w-8 p-1 duration-150"
-      />
-    </DisclosureButton>
-    <Transition
-      enter="transition duration-100 ease-out"
-      enterFrom="transform scale-95 opacity-0"
-      enterTo="transform scale-100 opacity-100"
-      leave="transition duration-75 ease-out"
-      leaveFrom="transform scale-100 opacity-100"
-      leaveTo="transform scale-95 opacity-0"
-    >
-      <DisclosurePanel>
-        He/They, and thanks in advance for respecting them!
-      </DisclosurePanel>
-    </Transition>
+  <Disclosure title="First off. Pronouns?">
+    He/They, and thanks in advance for respecting them!
   </Disclosure>
-  <Disclosure let:open class="space-y-2">
-    <DisclosureButton
-      class="flex justify-between min-w-full border-secondary border-2 rounded-sm pt-1 px-2"
-    >
-      <span>Any preference for titles?</span>
-      <ChevronUpIcon
-        style={open ? "transform: rotate(180deg);" : ""}
-        class="w-8 p-1 duration-150"
-      />
-    </DisclosureButton>
-    <Transition
-      enter="transition duration-100 ease-out"
-      enterFrom="transform scale-95 opacity-0"
-      enterTo="transform scale-100 opacity-100"
-      leave="transition duration-75 ease-out"
-      leaveFrom="transform scale-100 opacity-100"
-      leaveTo="transform scale-95 opacity-0"
-    >
-      <DisclosurePanel>Mr. works just fine!</DisclosurePanel>
-    </Transition>
+  <Disclosure title="Any preference for titles?">
+    Mr. works just fine!
   </Disclosure>
-  <Disclosure let:open class="space-y-2">
-    <DisclosureButton
-      class="flex justify-between min-w-full border-secondary border-2 rounded-sm pt-1 px-2"
-    >
-      <span>Okay! Anything you like?</span>
-      <ChevronUpIcon
-        style={open ? "transform: rotate(180deg);" : ""}
-        class="w-8 p-1 duration-150"
-      />
-    </DisclosureButton>
-    <Transition
-      enter="transition duration-100 ease-out"
-      enterFrom="transform scale-95 opacity-0"
-      enterTo="transform scale-100 opacity-100"
-      leave="transition duration-75 ease-out"
-      leaveFrom="transform scale-100 opacity-100"
-      leaveTo="transform scale-95 opacity-0"
-    >
-      <DisclosurePanel
-        >Cheesecakes of any and all varieties, elegant and <em>working</em> code,
-        and finally, sleep ;)
-      </DisclosurePanel>
-    </Transition>
+  <Disclosure title="Okay! Anything you like?">
+    Cheesecakes of any and all varieties, elegant and <em>working</em> code, and
+    finally, sleep ;)
   </Disclosure>
-  <Disclosure let:open class="space-y-2">
-    <DisclosureButton
-      class="flex justify-between min-w-full border-secondary border-2 rounded-sm pt-1 px-2"
-    >
-      <span>Anything you <em>dislike</em>?</span>
-      <ChevronUpIcon
-        style={open ? "transform: rotate(180deg);" : ""}
-        class="w-8 p-1 duration-150"
-      />
-    </DisclosureButton>
-    <Transition
-      enter="transition duration-100 ease-out"
-      enterFrom="transform scale-95 opacity-0"
-      enterTo="transform scale-100 opacity-100"
-      leave="transition duration-75 ease-out"
-      leaveFrom="transform scale-100 opacity-100"
-      leaveTo="transform scale-95 opacity-0"
-    >
-      <DisclosurePanel>
-        Not much, but for starters: Obtrusive and overly-explicit code (I see
-        you, java), heights, and those days where you just have nothing to do
-        but you want to do something, so you just walk around slightly agitated.
-      </DisclosurePanel>
-    </Transition>
+  <Disclosure title="Anything you <em>dislike</em>?">
+    Not much, but for starters: Obtrusive and overly-explicit code (I see you,
+    java), heights, and those days where you just have nothing to do but you
+    want to do something, so you just walk around slightly agitated.
   </Disclosure>
-  <Disclosure let:open class="space-y-2">
-    <DisclosureButton
-      class="flex justify-between min-w-full border-secondary border-2 rounded-sm pt-1 px-2"
-    >
-      <span>Oh! What's your setup at home?</span>
-      <ChevronUpIcon
-        style={open ? "transform: rotate(180deg);" : ""}
-        class="w-8 p-1 duration-150"
-      />
-    </DisclosureButton>
-    <Transition
-      enter="transition duration-100 ease-out"
-      enterFrom="transform scale-95 opacity-0"
-      enterTo="transform scale-100 opacity-100"
-      leave="transition duration-75 ease-out"
-      leaveFrom="transform scale-100 opacity-100"
-      leaveTo="transform scale-95 opacity-0"
-    >
-      <DisclosurePanel>
-        Glad you asked! Currently, I'm running a Ryzen 5 3600XT CPU with 16 GB
-        of RAM, along with a Radeon RX 6600 GPU, all on an MSI X470 GAMING PLUS
-        MAX ATX (oooh look fancy!) motherboard. And of course, it's running
-        Manjaro Linux. Because Linux > Windows.
-      </DisclosurePanel>
-    </Transition>
+  <Disclosure title="Oh! What's your setup at home?">
+    Glad you asked! Currently, I'm running a Ryzen 5 3600XT CPU with 16 GB of
+    RAM, along with a Radeon RX 6600 GPU, all on an MSI X470 GAMING PLUS MAX ATX
+    (oooh look fancy!) motherboard. And of course, it's running Manjaro Linux.
+    Because Linux > Windows.
   </Disclosure>
-  <Disclosure let:open class="space-y-2">
-    <DisclosureButton
-      class="flex justify-between min-w-full border-secondary border-2 rounded-sm pt-1 px-2"
-    >
-      <span>Anything you're working on right now?</span>
-      <ChevronUpIcon
-        style={open ? "transform: rotate(180deg);" : ""}
-        class="w-8 p-1 duration-150"
-      />
-    </DisclosureButton>
-    <Transition
-      enter="transition duration-100 ease-out"
-      enterFrom="transform scale-95 opacity-0"
-      enterTo="transform scale-100 opacity-100"
-      leave="transition duration-75 ease-out"
-      leaveFrom="transform scale-100 opacity-100"
-      leaveTo="transform scale-95 opacity-0"
-    >
-      <DisclosurePanel>
-        Nothing much, really. I'm not really doing anything besides making my
-        website cooler ;)
-      </DisclosurePanel>
-    </Transition>
+  <Disclosure title="Anything you're working on right now?">
+    Nothing much, really. I'm not really doing anything besides making my
+    website cooler ;)
   </Disclosure>
 </div>
 
