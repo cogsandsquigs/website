796e1c51976feeeb27241cd3b17b9a16befbe5c3
Ian Pratt <thingcoder1@gmail.com>
2022-05-25 13:36:10 +0000

    asdfasf

--- src/lib/components/BirthdayTimer.svelte
+++ src/lib/components/BirthdayTimer.svelte
@@ -1,30 +1,26 @@
-<script>
-  // @ts-nocheck
-  // have to do this bc svelte-countdown
-  // doesn't have ts bindings.
-  // TODO: implement own component
-
-  import Countdown from "svelte-countdown";
+<script lang="ts">
   import dayjs from "dayjs";
+
+  let now = dayjs();
+  let bday = dayjs(`${now.year()}-12-07`);
+
+  setInterval(() => {
+    now = dayjs();
+  }, 1000);
+
+  $: months = bday.diff(now, "month");
+  $: days = bday.diff(
+    dayjs(`${now.year()}-${now.month() + months}-${now.day()}`),
+    "day"
+  );
+  $: hours = bday.diff(now, "hour");
+  $: minutes = bday.diff(now, "minutes");
+  $: seconds = bday.diff(now, "seconds");
 </script>
 
-<Countdown
-  from={`${dayjs().year()}-12-07 00:00:00`}
-  dateFormat="YYYY-MM-DD H:m:s"
-  zone="America/Chicago"
-  let:remaining
->
-  <div class="whatever">
-    {#if remaining.done === false}
-      <span>{remaining.years} years</span>
-      <span>{remaining.months} months</span>
-      <span>{remaining.weeks} weeks</span>
-      <span>{remaining.days} days</span>
-      <span>{remaining.hours} hours</span>
-      <span>{remaining.minutes} minutes</span>
-      <span>{remaining.seconds} seconds</span>
-    {:else}
-      <span>The time has come!</span>
-    {/if}
-  </div>
-</Countdown>
+{months}
+{dayjs(`${now.year()}-${now.month() + months}-07`).toString()}
+{days}
+{hours}
+{minutes}
+{seconds}
