55089aa3519757eee4e3461ad63239124678c6b9
Ian Pratt <ianjdpratt@gmail.com>
2022-05-25 19:02:57 -0500

    yay added footer shit ig

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -10,7 +10,7 @@
 </svelte:head>
 
 <body
-  class="font-body text-md sm:text-lg md:text-xl bg-primary prose prose-invert prose-a:underline prose-a:decoration-secondary prose-a:decoration-2 prose-a:font-bold prose-hr:border-[1px] prose-hr:border-secondary max-w-none"
+  class="max-w-none min-h-screen font-body text-md sm:text-lg md:text-xl bg-primary prose prose-invert prose-a:underline prose-a:decoration-secondary prose-a:decoration-2 prose-a:font-bold prose-hr:border-[1px] prose-hr:border-secondary"
 >
   <Nav>
     <a href="/">Index</a>
@@ -21,16 +21,18 @@
     </Popover>
   </Nav>
 
-  <div class="px-2.5 mx-auto my-4 max-w-prose scrollbar-none">
+  <div class="min-h-[80vh] px-2.5 mx-auto my-4 max-w-prose scrollbar-none">
     <slot />
   </div>
 
-  <hr class="m-0" />
+  <div class="bottom-0 relative w-full">
+    <hr class="m-0" />
 
-  <p class="text-xs text-center">
-    This site was made with
-    <a href="https://kit.svelte.dev">SvelteKit</a> +
-    <a href="https://tailwindcss.com">Tailwind CSS</a>. It's deployed on
-    <a href="https://vercel.com">Vercel</a>.
-  </p>
+    <p class="text-xs text-center">
+      This site was made with
+      <a href="https://kit.svelte.dev">SvelteKit</a> +
+      <a href="https://tailwindcss.com">Tailwind CSS</a>. It's deployed on
+      <a href="https://vercel.com">Vercel</a>.
+    </p>
+  </div>
 </body>
