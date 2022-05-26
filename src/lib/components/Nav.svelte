36638d3c4e9257923c8cd43fcda54284d439f9a6
Ian Pratt <thingcoder1@gmail.com>
2022-05-26 18:28:21 +0000

    nav: gen. var name + default shows menu

--- src/lib/components/Nav.svelte
+++ src/lib/components/Nav.svelte
@@ -3,15 +3,15 @@
   import Popover from "$lib/components/Popover.svelte";
   import NavSubtitle from "$lib/components/NavSubtitle.svelte";
 
-  let w: number = 540;
-  const xs: number = 540; // screen size in pixels at when to break xs
+  let w: number = 0;
+  const breakpoint: number = 540; // screen size in pixels at when to break xs
 </script>
 
 <svelte:window bind:innerWidth={w} />
 
 <nav class="space-y-0 bg-primary">
   <div class="px-4 py-1 flex box-border justify-between items-center">
-    {#if w < xs}
+    {#if w < breakpoint}
       <Popover arrow={false}>
         <slot />
       </Popover>
