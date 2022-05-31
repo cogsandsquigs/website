cffd407bf2415620e380b49b04f3b4e1f0a3f1db
Ian Pratt <thingcoder1@gmail.com>
2022-05-31 17:40:54 +0000

    ok js works for index

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -9,6 +9,9 @@
       },
     };
   }
+
+  // generate js for this page
+  export const hydrate = true;
 </script>
 
 <script lang="ts">
@@ -30,24 +33,3 @@
 </div>
 
 <PostList posts={pages} />
-<!--
-<h1>Welcome to my website!!!</h1>
-
-<p>
-  I'm glad you could make it! This is my little corner of the web, made with
-  <a href="https://kit.svelte.dev">SvelteKit</a> and
-  <a href="https://tailwindcss.com">Tailwind CSS</a>. This website is also
-  deployed on
-  <a href="https://vercel.com">Vercel</a>! In any case, I hope you enjoy what
-  you find here! Why not check out my
-  <a href="blog/hello-world">testing page</a>? It's where I test out new things
-  for my website, and generally just mess around!
-</p>
-<p>
-  If you want to know more about me, go to my <a href="/about">about me page</a
-  >! Or, you could go explore the rest of my website. IDK, its up to you! After
-  all, I'm not paying a fortune for you to look at my site ;).
-</p>
-
-<p>Have fun, and stay snazzy!</p>
--->
