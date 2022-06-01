d6f85f7ced78766fee4d7cc4b44b24e2ae4e9a71
Ian Pratt <thingcoder1@gmail.com>
2022-06-01 14:08:53 -0500

    rrrr things

---
+++ src/routes/testing/mem/components/Card.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+  export let front: string = "Hello";
+  export let back: string = "World!";
+</script>
+
+<div class="bg-gradient-to-tr from-secondary to-accent rounded-xl w-52 h-72">
+  <div class="front flex flex-1 justify-center items-center mb-6 w-full h-full">
+    <h3 class="m-0">{front}</h3>
+  </div>
+</div>
---
+++ src/routes/testing/mem/index.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+  import Card from "./components/Card.svelte";
+</script>
+
+<h1>Mem</h1>
+
+<Card />
