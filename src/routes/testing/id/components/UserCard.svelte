68fc8ef94994c5f9cbb1c69544cc5ac51ff03778
Ian Pratt <thingcoder1@gmail.com>
2022-06-02 10:30:54 -0500

    rrr

---
+++ src/routes/testing/id/components/UserCard.svelte
@@ -0,0 +1,2 @@
+<div class="w-60 h-80 border-2 border-secondary rounded-sm">
+</div>
--- src/routes/testing/id/index.svelte
+++ src/routes/testing/id/index.svelte
@@ -1,5 +1,7 @@
+<script lang="ts">
+	import UserCard from "./components/UserCard.svelte";
+</script>
+
 <h1>ID</h1>
 
-<form>
-	<input type="text" class="rounded-sm " />
-</form>
+<UserCard />
