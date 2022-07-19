99e8c08c24c72fc286137849e70529faee804146
Ian Pratt <ianjdpratt@gmail.com>
2022-07-19 13:23:30 -0500

    changes plz work

--- svelte.config.js
+++ svelte.config.js
@@ -11,11 +11,11 @@ const config = {
             // if true, will deploy the app using edge functions
             // (https://vercel.com/docs/concepts/functions/edge-functions)
             // rather than serverless functions
-            edge: false,
+            edge: true,
 
             // an array of dependencies that esbuild should treat
             // as external when bundling functions
-            external: [],
+            external: ["canvas"],
 
             // if true, will split your app into multiple functions
             // instead of creating a single one for the entire app
