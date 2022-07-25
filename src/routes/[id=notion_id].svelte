65c1586064bd90d4f9fed5d9579030364baea0d4
Ian Pratt <ianjdpratt@gmail.com>
2022-07-25 16:01:11 -0500

    TODO: redirection to notion subpage for notion ids

---
+++ src/routes/[id=notion_id].svelte
@@ -0,0 +1,46 @@
+<script lang="ts" context="module">
+    import { Notion } from "$lib/notion";
+    import { APIErrorCode } from "@notionhq/client";
+
+    export const load = async ({ params }) => {
+        let page: any;
+
+        try {
+            page = await Notion.pages.retrieve({ page_id: params.id });
+        } catch (error) {
+            if (error.code === APIErrorCode.ObjectNotFound) {
+                return {
+                    status: 404,
+                    error: `Not found: /${params.id}`,
+                };
+            } else {
+                return {
+                    status: 500,
+                    error: error,
+                };
+            }
+        }
+
+        if (
+            Object.entries(page.properties).filter(([key]) => key === "slug")
+                .length !== 0
+        ) {
+            return {
+                status: 301,
+                redirect: `/${
+                    (
+                        (await Notion.pages.properties.retrieve({
+                            page_id: page.id,
+                            property_id: "RAnF",
+                        })) as any
+                    ).results[0].rich_text.text.content
+                }`,
+            };
+        } else {
+            return {
+                status: 302,
+                redirect: `/notion/${page.id}`,
+            };
+        }
+    };
+</script>
