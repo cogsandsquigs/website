21b5e97d881ed49ffa2c3fa64c5001eb4108bb4f
Ian Pratt <ianjdpratt@gmail.com>
2022-08-01 22:03:44 -0500

    changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+    content: ["./**/*.{html,md,css,js}"],
+    theme: {
+        extend: {
+            colors: {
+                primary: "#111827",
+                secondary: "#3ABFF8",
+                accent: "#2563EB",
+                neutral: "#111827",
+                "base-100": "#111827",
+                info: "#3ABFF8",
+                success: "#36D399",
+                warning: "#FBBD23",
+                error: "#F87272",
+            },
+        },
+    },
+
+    extend: {
+        screens: {
+            xs: "540px",
+        },
+    },
+
+    plugins: [require("@tailwindcss/typography")],
+};
