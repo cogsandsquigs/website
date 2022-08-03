43086a3d3b7543cbc336fd5b46e0a95883951860
Ian Pratt <ianjdpratt@gmail.com>
2022-08-02 21:02:04 -0500

    changes

--- themes/cogs/postcss.config.js
+++ themes/cogs/postcss.config.js
@@ -1,6 +1,9 @@
 module.exports = {
     plugins: [
-        require("postcss-import"),
+        require("postcss-import")({
+            root: "themes/cogs",
+            path: ["assets/css"],
+        }),
         require("tailwindcss")("themes/cogs/tailwind.config.js"),
         require("autoprefixer"),
     ],
