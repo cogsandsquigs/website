cafac48adc6f63aa87c652800ff7159b1208b1af
Ian Pratt <ianjdpratt@gmail.com>
2022-08-14 18:19:42 +0000

    changes

---
+++ src/lib/components/Footer.svelte
@@ -0,0 +1,3 @@
+<hr class="bottom-0 m-0 mb-2" />
+
+<footer class="flex bottom-0 justify-center mb-2 w-full text-xs" />
--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -3,6 +3,7 @@
     import "victormono";
 
     import Header from "$lib/components/Header.svelte";
+    import Footer from "$lib/components/Footer.svelte";
 </script>
 
 <Header />
@@ -11,6 +12,4 @@
     <slot />
 </div>
 
-<hr class="bottom-0 m-0 mb-2" />
-
-<footer class="flex bottom-0 justify-center mb-2 w-full text-xs" />
+<Footer />
