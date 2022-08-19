00540b1c11d0b69e5f120c89ea1ab533ee8c1545
Ian Pratt <ianjdpratt@gmail.com>
2022-08-18 20:03:14 -0500

    change ig

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,11 +1,15 @@
+import type { Page } from "$lib/types";
+
 // See https://kit.svelte.dev/docs/types#app
 // for information about these interfaces
 // and what to do when importing types
-declare namespace App {
-	// interface Locals {}
-	// interface Platform {}
-	// interface PrivateEnv {}
-	// interface PublicEnv {}
-	// interface Session {}
-	// interface Stuff {}
+declare global {
+    namespace App {
+        // interface Locals {}
+        // interface Platform {}
+        // interface PrivateEnv {}
+        // interface PublicEnv {}
+        // interface Session {}
+        // interface Stuff {}
+    }
 }
