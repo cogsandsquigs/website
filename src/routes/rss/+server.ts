f55225eaf1dd2b648a710c164af2f66bf44b0224
Ian Pratt <ianjdpratt@gmail.com>
2022-08-18 21:55:54 -0500

    prerender

--- src/routes/rss/+server.ts
+++ src/routes/rss/+server.ts
@@ -1,3 +1,5 @@
+export const prerender = true;
+
 import { all } from "$lib/all";
 import type { Page } from "$lib/types";
 import type { PageLoad } from "./$types";
@@ -7,7 +9,6 @@ const base = "https://cogsandsquigs.gq";
 export const GET: PageLoad = async () => {
     const body = render(await all());
     const headers = {
-        "Cache-Control": `max-age=0, s-max-age=${600}`,
         "Content-Type": "application/xml",
     };
     return new Response(body || "", { headers });
@@ -17,7 +18,7 @@ const render = (posts: Page[]) => `<?xml version="1.0" encoding="UTF-8" ?>
   <rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom">
   <channel>
   <atom:link href="${base}/rss" rel="self" type="application/rss+xml" />
-  <title>David W Parker</title>
+  <title>Cogs and Squigs</title>
   <link>${base}</link>
   <description>My little slice of the blogosphere</description>
   ${posts
