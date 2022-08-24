a06327441a65a4e9729f5463e7d4b03cb7f7183b
Ian Pratt <ianjdpratt@gmail.com>
2022-08-23 21:54:18 -0500

    yay more changes

--- src/routes/tags/[tag]/+page.ts
+++ src/routes/tags/[tag]/+page.ts
@@ -4,16 +4,9 @@ import type { PageLoad } from "./$types";
 export const prerender = true;
 
 export const load: PageLoad = async ({ params }) => {
-    let filteredPosts = await all().then((pages) => {
-        return pages.filter((page) => {
-            console.log(
-                page.frontmatter.title,
-                page.frontmatter.tags.includes(params.tag)
-            );
-            return page.frontmatter.tags.includes(params.tag);
-        });
-    });
-
+    let filteredPosts = await all().then((pages) =>
+        pages.filter((page) => page.frontmatter.tags.includes(params.tag))
+    );
     return {
         title: params.tag,
         posts: filteredPosts,
