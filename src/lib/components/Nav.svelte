164b4f67606fda47faf31f115c56db3a62bd38fe
Ian Pratt <ianjdpratt@gmail.com>
2022-08-28 17:47:21 +0000

    changes ig

--- src/lib/components/Nav.svelte
+++ src/lib/components/Nav.svelte
@@ -1,5 +1,7 @@
 <nav class="mb-4">
-    <div class="grid grid-cols-3 sm:grid-cols-5 my-2 [&>*]:w-min [&>*]:mx-auto">
+    <div
+        class="grid grid-cols-3 sm:grid-cols-5 my-2 [&>*]:w-min [&>*]:my-4 [&>*]:items-center [&>*]:mx-auto"
+    >
         <slot />
     </div>
 
