37534451ebf4d14ab48727073bc040a48492f884
Ian Pratt <ianjdpratt@gmail.com>
2022-09-23 17:52:17 -0500

    test?

--- svelte.config.js
+++ svelte.config.js
@@ -15,7 +15,14 @@ export default {
     ],
 
     kit: {
-        adapter: adapter(),
+        adapter: adapter({
+            // default options are shown. On some platforms
+            // these options are set automatically — see below
+            pages: "build",
+            assets: "build",
+            fallback: true,
+            precompress: false,
+        }),
 
         prerender: {
             crawl: true,
