be791bd07ab1badefeda8094cb9d9241a20602cc
Ian Pratt <ianjdpratt@gmail.com>
2022-10-06 12:31:13 +0000

    yar

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from "@sveltejs/adapter-static";
+import adapter from "@sveltejs/adapter-vercel";
 import preprocess from "svelte-preprocess";
 import { mdsvex } from "mdsvex";
 import mdsvexConfig from "./mdsvex.config.js";
