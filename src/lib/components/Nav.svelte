db0d3f32f79f19e5b3865ab382c341ed683eafb4
Ian Pratt <ianjdpratt@gmail.com>
2022-11-03 21:06:51 +0000

    Update Nav.svelte and links.scss

--- src/lib/components/Nav.svelte
+++ src/lib/components/Nav.svelte
@@ -1,10 +1,17 @@
-<nav class="px-4">
-	<div class="flex">
+<nav class="px-4 py-2 space-y-2">
+	<div class="flex align-items-center">
+		<h1 class="text-2xl">Cogs and Squigs</h1>
+		
+		<a>test</a>
 		<a>test</a>
 	</div>
 
 	<!-- Bottom line across screen -->
 	<hr
-		class="gradient my-2 h-0.5 border-none rounded-sm"
+		class="gradient h-0.5 border-none rounded-sm"
 	/>
 </nav>
+
+<style>
+	
+</style>
