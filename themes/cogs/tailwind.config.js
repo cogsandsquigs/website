a37a1f27e78e79d5ffb211618b479d9b23ceda4b
Ian Pratt <ianjdpratt@gmail.com>
2022-11-27 17:31:09 +0000

    convert css to tailwind

--- themes/cogs/tailwind.config.js
+++ themes/cogs/tailwind.config.js
@@ -1,9 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ["content/**/*.md", "layouts/**/*.html"],
+	content: ["content/**/*.md", "**/layouts/**/*.html"],
 
 	theme: {
-		extend: {}
+		extend: {
+			colors: {
+				// The primary color
+				primary: "#30adff",
+
+				// The secondary color
+				secondary: "#6a3cff",
+
+				// The accent/complement to the primary and secondary colors
+				accent: "#ff23ff",
+
+				// The base/background color
+				base: "#131315"
+			}
+		}
 	},
-	plugins: []
+	plugins: [require("@tailwindcss/typography"), require("@tailwindcss/line-clamp")]
 };
