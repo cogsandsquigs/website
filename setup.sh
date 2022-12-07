c7dcb48426a741801215db7d5c7e261437aee551
Ian Pratt <ianjdpratt@gmail.com>
2022-12-07 18:03:29 +0000

    newwer hugo version

--- setup.sh
+++ setup.sh
@@ -1,10 +1,10 @@
 #! /bin/bash
 
 # Get the hugo binary
-wget https://github.com/gohugoio/hugo/releases/download/v0.106.0/hugo_extended_0.106.0_linux-amd64.deb
+wget https://github.com/gohugoio/hugo/releases/download/v0.108.0/hugo_extended_0.108.0_linux-amd64.deb
 
 # Install the hugo binary
-sudo apt install ./hugo_extended_0.106.0_linux-amd64.deb
+sudo apt install ./hugo_extended_0.108.0_linux-amd64.deb
 
 # Remove the hugo binary
-rm hugo_extended_0.106.0_linux-amd64.deb
+rm hugo_extended_0.108.0_linux-amd64.deb
