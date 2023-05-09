4e27988dd36615afa164b116fc248d2ccd6b5610
Ian Pratt <ianjdpratt@gmail.com>
2023-05-09 21:31:02 +0000

    almost done!

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,25 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 const config = {
-	content: ['./src/**/*.{html,js,svelte,ts}'],
+	content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
 
 	theme: {
 		extend: {
 			colors: {
 				// The primary color
-				primary: '#30adff',
+				primary: "#30adff",
 
 				// The secondary color
-				secondary: '#6a3cff',
+				secondary: "#6a3cff",
 
 				// The accent/complement to the primary and secondary colors
-				accent: '#ff23ff',
+				accent: "#ff23ff",
 
 				// The base/background color
-				base: '#131315'
+				base: "#131315"
 			}
 		}
 	},
-	plugins: [require('@tailwindcss/typography')]
+	plugins: [require("@tailwindcss/typography")]
 };
 
 module.exports = config;
