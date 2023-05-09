bf60aed2a4728b47f99f121aeea64916a4e61965
Ian Pratt <ianjdpratt@gmail.com>
2023-05-08 20:42:55 -0500

    add more stuff + config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,7 +3,6 @@ module.exports = {
 	extends: [
 		"eslint:recommended",
 		"plugin:@typescript-eslint/recommended",
-		"plugin:svelte/recommended",
 		"prettier"
 	],
 	parser: "@typescript-eslint/parser",
