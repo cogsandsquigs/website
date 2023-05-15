5aa4545f9180344c134bbb0719d0cf66119115b5
Ian Pratt <ianjdpratt@gmail.com>
2023-05-15 20:14:54 +0000

    chanegs

--- src/components/IconifyIcon.svelte
+++ src/components/IconifyIcon.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	// Code for this came from https://docs.iconify.design/api/svg.html
+	// API for this came from https://docs.iconify.design/api/svg.html
 
 	/**
 	 * The icon set this icon belongs to.
@@ -12,26 +12,31 @@
 	export let name: string;
 
 	/**
+	 * The color of the icon.
+	 */
+	export let color: string | undefined = undefined;
+	color = color ? color : "";
+
+	/**
 	 * The width of the icon. Numbers without units are treaded as pixel measurements.
 	 */
-	export let width: string | number | undefined = "1em";
+	export let width: string | number | undefined = undefined;
+	width = width ? width : "";
 
 	/**
 	 * The height of the icon. Numbers without units are treated as pixel measurements.
 	 */
-	export let height: string | number | undefined = width;
+	export let height: string | number | undefined = undefined;
+	height = height ? height : "";
 
 	/**
 	 * Any optional CSS classes passed to the icon.
 	 */
-	export let classes: string = "";
-
-	if (!width && !height) {
-	}
+	export let styles: string = "";
 </script>
 
 <img
-	class={classes}
-	src={`https://api.iconify.design/${set}/${name}.svg`}
+	class={styles}
+	src={`https://api.iconify.design/${set}/${name}.svg?color=${color}&width=${width}&height=${height}`}
 	alt={`The ${name} icon from the ${set} icon set.`}
 />
