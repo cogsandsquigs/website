13a6543e1d607dc2f9129229d4cd4c6ffa3f496c
Ian Pratt <ianjdpratt@gmail.com>
2023-06-13 19:13:58 -0500

    add tina schema

---
+++ tina/__generated__/client.ts
@@ -0,0 +1,5 @@
+import { createClient } from "tinacms/dist/client";
+import { queries } from "./types";
+export const client = createClient({ url: 'https://content.tinajs.io/1.4/content/c591f1cb-333e-43b2-bf08-9598be096e5e/github/main', token: 'be1486960cf6533fb318339f7b8cec7971cc4009', queries });
+export default client;
+  
