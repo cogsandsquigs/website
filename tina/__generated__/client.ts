6f6203b45a6e5e332c74660c471fbcc246aad986
Ian Pratt <ianjdpratt@gmail.com>
2023-06-14 21:56:11 -0500

    changes

--- tina/__generated__/client.ts
+++
@@ -1,5 +0,0 @@
-import { createClient } from "tinacms/dist/client";
-import { queries } from "./types";
-export const client = createClient({ url: 'http://localhost:4001/graphql', token: 'be1486960cf6533fb318339f7b8cec7971cc4009', queries });
-export default client;
-  
