7c03035b6f37fbcaced6767a9edf6cf46826a315
Ian Pratt <ianjdpratt@gmail.com>
2023-11-25 19:57:49 -0600

    moar upds

--- __previous_astro_site/src/components/Tag.svelte
+++
@@ -1,8 +0,0 @@
-<script lang="ts">
-	export let tag: string;
-</script>
-
-<a
-	class="tag text-primary font-bold border-primary rounded-full decoration-none border-[1px] break-words px-2 mr-2 no-underline"
-	href={`/tags/${tag}`}>{tag}</a
->
