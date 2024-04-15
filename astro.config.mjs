be94e7a174e18743a745869fd989d74987f8798b
Ian Pratt <ianjdpratt@gmail.com>
2024-04-15 15:08:17 -0500

    asdf

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,13 @@
+import sitemap from "@astrojs/sitemap";
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+	// The URL of my website
+	site: "https://cogsandsquigs.dev",
+
+	// Integrations / plugins that make my website work better
+	integrations: [
+		sitemap(), // Generates a sitemap.xml file for SEO
+	],
+});
