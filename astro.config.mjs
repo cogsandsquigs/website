497672cbe9b9a1da343613cbb821ac34b72c6d7c
Ian Pratt <ianjdpratt@gmail.com>
2024-05-01 22:34:21 -0500

    upds

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,20 +1,23 @@
 import sitemap from "@astrojs/sitemap";
-import { defineConfig, squooshImageService } from "astro/config";
-
 import tailwind from "@astrojs/tailwind";
+import { defineConfig, squooshImageService } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  // The URL of my website
-  site: "https://cogsandsquigs.dev",
-  // Image optimization service
-  image: {
-    // Use Squoosh for image optimization instead of the default Sharp service as Sharp is not available with Bun
-    service: squooshImageService({
-      webp: true
-    })
-  },
-  // Integrations / plugins that make my website work better
-  integrations: [sitemap() // Generates a sitemap.xml file for SEO
-  , tailwind()]
+	// The URL of my website
+	site: "https://cogsandsquigs.dev",
+
+	// Image optimization service
+	image: {
+		// Use Squoosh for image optimization instead of the default Sharp service as Sharp is not available with Bun
+		service: squooshImageService({
+			webp: true,
+		}),
+	},
+
+	// Integrations / plugins that make my website work better
+	integrations: [
+		sitemap(), // Generates a sitemap.xml file for SEO
+		tailwind(), // Adds Tailwind CSS support
+	],
 });
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
 	theme: {
+		colors: {
+			// Thematic colors
+			primary: "#30ADFF",
+			secondary: "#6A3CFF",
+			accent: "#FF23FF",
+
+			// Background colors
+			base: {
+				DEFAULT: "#131315",
+				light: "#5B5B65",
+			},
+
+			// Foreground/Text colors
+			foreground: {
+				DEFAULT: "#D1D5DB",
+			},
+		},
+
 		extend: {},
 	},
 	plugins: [],
-}
+};
