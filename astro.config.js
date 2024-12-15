d10d66699ea946b744e2fca9d4761229f1ba703a
Ian Pratt <ianjdpratt@gmail.com>
2024-12-15 22:00:57 +0000

    aaa

--- astro.config.js
+++ astro.config.js
@@ -1,11 +1,11 @@
 import { defineConfig } from "astro/config";
 // import mdx from "@astrojs/mdx";
-// import sitemap from "@astrojs/sitemap";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
     site: "https://cogsandsquigs.dev",
-    // integrations: [mdx(), sitemap()],
+    integrations: [/* mdx(), */ sitemap()],
     markdown: {
         shikiConfig: {
             theme: "catppuccin-mocha",
