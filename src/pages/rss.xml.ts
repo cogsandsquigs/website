f1dafca88502e1c1303d614a0bde68729f9bb457
Ian Pratt <ianjdpratt@gmail.com>
2025-01-15 10:28:38 +0000

    add cv + other things

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -1,16 +1,17 @@
 import rss from "@astrojs/rss";
 import { getCollection } from "astro:content";
 import { SITE_TITLE, SITE_DESCRIPTION } from "../consts";
+import type { APIRoute } from "astro";
 
-export async function GET(context) {
+export const GET: APIRoute = async (context) => {
     const posts = await getCollection("blog");
     return rss({
         title: SITE_TITLE,
         description: SITE_DESCRIPTION,
-        site: context.site,
+        site: context.site ?? "",
         items: posts.map((post) => ({
             ...post.data,
-            link: `/blog/${post.slug}/`,
+            link: `/blog/${post.id}/`,
         })),
     });
-}
+};
