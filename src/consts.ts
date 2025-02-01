746d8eae17ccfc9ef89e5bad80a83013f9f7d56d
Ian Pratt <ianjdpratt@gmail.com>
2025-02-01 02:34:51 +0000

    add moar thing

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,10 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
+import { execSync } from "child_process";
+
 export const SITE_TITLE = "Cogs And Squigs";
 export const SITE_DESCRIPTION = "My little slice of the blogosphere";
+export const COMMIT_HASH_SHORT = execSync("git rev-parse --short HEAD")
+    .toString()
+    .trim();
