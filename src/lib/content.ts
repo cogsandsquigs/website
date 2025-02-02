5b79e70ea847a3e9d6ac03feab7967670ee1f810
Ian Pratt <ianjdpratt@gmail.com>
2025-02-02 17:07:15 +0000

    upds

--- src/lib/content.ts
+++ src/lib/content.ts
@@ -1,7 +1,7 @@
 import { getCollection } from "astro:content";
 
 export const posts = (await getCollection("blog"))
-    .filter((p) => !p.data.draft || import.meta.env.MODE == "development")
+    .filter((p) => !p.data.draft || import.meta.env.DEV)
     .sort((a, b) => b.data.date.getTime() - a.data.date.getTime());
 
 export const projects = (await getCollection("projects"))
