8dbfb1d907344f77f82e1a82a4c08d8bc77b792d
Ian Pratt <ianjdpratt@gmail.com>
2026-01-24 15:24:01 +0000

    cleanups

--- README.md
+++ README.md
@@ -1,21 +1,7 @@
-[![Online?](https://img.shields.io/website?url=https%3A%2F%2Fcogsandsquigs.dev&style=for-the-badge&link=https%3A%2F%2Fcogsandsquigs.dev)
-](https://cogsandsquigs.dev)
+[![Online?](https://img.shields.io/website?url=https%3A%2F%2Fcogsandsquigs.dev&style=for-the-badge&link=https%3A%2F%2Fcogsandsquigs.dev)](https://cogsandsquigs.dev)
 
 # My website
 
 This is my personal website, and accessable [here](https://cogsandsquigs.dev)!
 
 Built with Zola and love <3
-
-## Notes for people looking at this
-
-- `/tina` holds all the [TinaCMS configuration files](https://tina.io/docs/tina-folder/overview/) for the content in this site.
-- The structure of this repository follows [the standard Zola project structure](https://www.getzola.org/documentation/getting-started/directory-structure/).
-
-## References and Tools
-
-- [Animista](https://animista.net)
-
-## TODOs
-
-- [ ] Fix footers
--- flake.nix
+++ flake.nix
@@ -1,25 +1,21 @@
 {
     # Flake inputs
     inputs = {
-        nixpkgs.url = "github:NixOS/nixpkgs"; # Latest stable Nixpkgs
-        flake-utils.url = "github:numtide/flake-utils";
+        nixpkgs.url = "github:NixOS/nixpkgs?ref=cf2f56f17dba3f894f754739c36c8465a40ba513";
+        flake-utils.url = "github:numtide/flake-utils?ref=11707dc2f618dd54ca8739b309ec4fc024de578b";
     };
 
     # Flake outputs
     # Development environment output
-    outputs = {
-        nixpkgs,
-        flake-utils,
-        ...
-    }:
-        flake-utils.lib.eachDefaultSystem (system: let
-            pkgs = import nixpkgs {inherit system;};
-        in {
-            devShells.default = pkgs.mkShell {
-                packages = with pkgs; [
-                    # bun
-                    zola
-                ];
-            };
-        });
+    outputs =
+        { nixpkgs, flake-utils, ... }:
+        flake-utils.lib.eachDefaultSystem (
+            system:
+            let
+                pkgs = import nixpkgs { inherit system; };
+            in
+            {
+                devShells.default = pkgs.mkShell { packages = with pkgs; [ zola ]; };
+            }
+        );
 }
