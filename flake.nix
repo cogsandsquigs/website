e9ba6d3e4fc1c30e98bdbe0b0d3763b6a0ef6c52
Ian Pratt <ianjdpratt@gmail.com>
2026-09-17 18:28:05 +0100

    use nixpkgs-multiverse

--- flake.nix
+++ flake.nix
@@ -1,24 +1,31 @@
 {
     # Flake inputs
     inputs = {
-        nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+        nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
+        multiverse.url = "github:fzakaria/nixpkgs-multiverse";
         flake-utils.url = "github:numtide/flake-utils";
     };
 
     # Flake outputs
     # Development environment output
     outputs =
-        { nixpkgs, flake-utils, ... }:
+        {
+            nixpkgs,
+            flake-utils,
+            multiverse,
+            ...
+        }:
         flake-utils.lib.eachDefaultSystem (
             system:
             let
                 pkgs = import nixpkgs { inherit system; };
+                mv = multiverse.multiverse.${system};
 
-                buildPackages = with pkgs; [ zola ];
+                buildPackages = [ (mv.version "zola" "0.23.6") ];
 
-                devPackages = with pkgs; [
-                    twig-language-server
-                    ludtwig
+                devPackages = [
+                    mv.latest.twig-language-server
+                    mv.latest.ludtwig
                 ];
             in
             {
